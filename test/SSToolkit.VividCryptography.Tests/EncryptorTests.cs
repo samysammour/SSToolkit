ffd5c3550a9925175b2f7add22908c60c2110172
s.sammour <samy.sammour@hotmail.com>
2022-02-16 16:15:33 +0100

    Add project files.

---
+++ test/SSToolkit.VividCryptography.Tests/EncryptorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace SSToolkit.VividCryptography.Tests
+{
+    using SSToolkit.VividCryptography;
+    using Xunit;
+
+    public class EncryptorTests
+    {
+        [Fact]
+        public void EncryptDecrypt_Test()
+        {
+            string plainText = "4242424242424242";
+            var crypto = new Encryptor("ASDasd@!#!@#SDASD@E!@D!WDQDASDADR#%^GFGHGSDGTJMUIP12312541235423");
+            var cipherText = crypto.Encrypt(plainText);
+            var result = crypto.Decrypt(cipherText);
+            Assert.Equal(result, plainText);
+
+            var cipherText2 = crypto.Encrypt(plainText);
+            var result2 = crypto.Decrypt(cipherText2);
+            Assert.Equal(result2, plainText);
+        }
+    }
+}
---
+++ test/SSToolkit.VividCryptography.Tests/HashingTests.cs
@@ -0,0 +1,65 @@
+﻿namespace SSToolkit.VividCryptography.Tests
+{
+    using SSToolkit.VividCryptography;
+    using Xunit;
+    
+    public class HashingTests
+    {
+        [Fact]
+        public void GetSaltTest()
+        {
+            var hashing = new Hashing();
+            var salt = hashing.GetSalt();
+
+            Assert.NotNull(salt);
+            Assert.True(salt.Length == 32);
+
+            var repeatSalt = hashing.GetSalt();
+            Assert.NotEqual(salt, repeatSalt);
+        }
+
+        [Fact]
+        public void GetCipherTextTest()
+        {
+            string plainText = "Hello world!";
+            var hashing = new Hashing();
+            // not empty
+            var salt = hashing.GetSalt();
+            var cipher = hashing.GetCipherText(plainText, salt);
+            Assert.True(!string.IsNullOrEmpty(cipher));
+            Assert.True(cipher.Length == 512);
+
+            // the same result always
+            var repeatCipher = hashing.GetCipherText(plainText, salt);
+            Assert.Equal(cipher, repeatCipher);
+
+            // cannot be duplicated
+            var anotherCipher = hashing.GetCipherText(plainText + "s", salt);
+            Assert.NotEqual(cipher, anotherCipher);
+
+            // not the same with new salt
+            var newSalt = hashing.GetSalt();
+            var newCipher = hashing.GetCipherText(plainText, newSalt);
+            Assert.NotEqual(cipher, newCipher);
+        }
+
+        [Fact]
+        public void CompateHashTest()
+        {
+            string plainText = "Hello world!";
+            var hashing = new Hashing();
+            // match
+            var salt = hashing.GetSalt();
+            var oldHash = hashing.GetCipherText(plainText, salt);
+            Assert.True(hashing.CompareHash(plainText, oldHash, salt));
+
+            // different salt
+            var anotherSalt = hashing.GetSalt();
+            Assert.False(hashing.CompareHash(plainText, oldHash, anotherSalt));
+
+            // different hash
+            var anotherHash = hashing.GetCipherText(plainText, anotherSalt);
+            Assert.False(hashing.CompareHash(plainText, anotherHash, salt));
+        }
+    }
+}
